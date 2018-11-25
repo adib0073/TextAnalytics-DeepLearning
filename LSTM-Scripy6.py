0205521d00e6dc413708325688a08ab1ad588639
adib0073 <adib0073@gmail.com>
2018-11-25 17:30:14 +0530

    Update LSTM-Scripy6.py

--- LSTM-Scripy6.py
+++ LSTM-Scripy6.py
@@ -1,1 +1,13 @@
 
+from keras.layers import LSTM
+model = Sequential()
+model.add(Embedding(max_features, 32))
+model.add(LSTM(32))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop',
+loss='binary_crossentropy',
+metrics=['acc'])
+history = model.fit(input_train, y_train,
+epochs=10,
+batch_size=128,
+validation_split=0.2)
