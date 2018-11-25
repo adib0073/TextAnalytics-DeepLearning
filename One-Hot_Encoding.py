6dcb3125f45b951dc8c811e8587f35c8c6032ea9
adib0073 <adib0073@gmail.com>
2018-11-25 12:01:59 +0530

    Update One-Hot_Encoding.py

--- One-Hot_Encoding.py
+++ One-Hot_Encoding.py
@@ -31,6 +31,26 @@ for i, sample in enumerate(samples):
     for j, character in enumerate(sample):
         index = token_index.get(character)
         results[i, j, index] = 1
+       
+# One Hot Encoding using Keras
+from keras.preprocessing.text import Tokenizer
+samples = ['The cat sat on the mat.', 'The dog ate my homework.']
+tokenizer = Tokenizer(num_words=1000)
+tokenizer.fit_on_texts(samples)
+sequences = tokenizer.texts_to_sequences(samples)
+one_hot_results = tokenizer.texts_to_matrix(samples, mode='binary')
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+
+#Word level one-hot encoding with Hashing
+samples = ['The cat sat on the mat.', 'The dog ate my homework.']
+dimensionality = 1000
+max_length = 10
+results = np.zeros((len(samples), max_length, dimensionality))
+for i, sample in enumerate(samples):
+    for j, word in list(enumerate(sample.split()))[:max_length]:
+        index = abs(hash(word)) % dimensionality
+        results[i, j, index] = 1
         
-        
+
  
