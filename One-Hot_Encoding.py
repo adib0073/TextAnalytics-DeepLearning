53f5c46ef5129797b4834b40cc2e9181c63fafbe
adib0073 <adib0073@gmail.com>
2018-11-25 11:45:08 +0530

    Update One-Hot_Encoding.py

--- One-Hot_Encoding.py
+++ One-Hot_Encoding.py
@@ -18,3 +18,19 @@ for i, sample in enumerate(samples):
         
 print(token_index) # {'World': 2, 'Hello': 1, 'you.': 5, 'are': 4, 'How': 3}
 results.shape #(2, 10, 6)  ## ( num_sentences,max_length,total_token_indices)
+
+# Character-level one-hot encoding
+
+import string
+samples = ['Hello World', 'How are you.']
+characters = string.printable   #All printable ASCII characters
+token_index = dict(zip(range(1, len(characters) + 1), characters))
+max_length = 50
+results = np.zeros((len(samples), max_length, max(token_index.keys()) + 1))
+for i, sample in enumerate(samples):
+    for j, character in enumerate(sample):
+        index = token_index.get(character)
+        results[i, j, index] = 1
+        
+        
+ 
